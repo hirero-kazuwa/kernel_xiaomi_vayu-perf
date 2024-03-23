82f18c888e9ba7863927b15115618d0a4d75dbd0
hirero-kazuwa <hirero00@gmail.com>
2024-03-23 09:23:48 +0530

    vayu: add scripts && use python3 script credits https://github.com/SkylineUI-Devices/kernel_xiaomi_vayu/blob/fourteen/build.sh

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Mar 23 09:18:35 IST 2024"
+#define LINUX_COMPILE_BY "home"
+#define LINUX_COMPILE_HOST "gxc2356"
+#define LINUX_COMPILER "Android (11368308, +pgo, +bolt, +lto, +mlgo, based on r510928) clang version 18.0.0 (https://android.googlesource.com/toolchain/llvm-project 477610d4d0d988e69dbc3fae4fe86bff3f07f2b5), LLD 18.0.0"
---
+++ vayu.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Cleaning old stuff"
+
+LGR='\033[1;32m'
+
+make clean
+make mrproper
+cd out
+rm -rf * -x AnyKernel3
+cd ..
+echo -e ${LGR} "#### clean has been completed successfully  ####"
+
+kernel_dir="${PWD}"
+CCACHE=$(command -v ccache)
+objdir="${kernel_dir}/out"
+builddir="${kernel_dir}/build"
+ZIMAGE=$kernel_dir/out/arch/arm64/boot/Image
+
+# Modify the Path according to your location
+TC_DIR=/home/hirero/e/clang/p
+CLANG_DIR=/home/hirero/e/clang/clang
+##
+
+export CONFIG_FILE="vayu_user_defconfig"
+export ARCH="arm64"
+export KBUILD_BUILD_USER=home
+
+export PATH="$CLANG_DIR/bin:$PATH"
+
+    START=$(date +"%s")
+    echo -e ${LGR} "########### Generating Defconfig ############${NC}"
+    make -s ARCH=${ARCH} O=${objdir} ${CONFIG_FILE} -j$(nproc --all)
+
+    cd ${kernel_dir}
+    echo -e ${LGR} "######### Compiling kernel #########${NC}"
+    make -j$(nproc --all) \
+    O=out \
+    ARCH=${ARCH}\
+    CC="ccache clang" \
+    CLANG_TRIPLE="aarch64-linux-gnu-" \
+    CROSS_COMPILE="aarch64-linux-gnu-" \
+    CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
+    LLVM=1 \
+    LLVM_IAS=1
+
+    cd ${objdir}
+    COMPILED_IMAGE=arch/arm64/boot/Image
+    COMPILED_DTBO=arch/arm64/boot/dtbo.img
+    if [[ -f ${COMPILED_IMAGE} && ${COMPILED_DTBO} ]]; then
+
+        echo -e ${LGR} "#### build completed successfully (hh:mm:ss) ####"
+    else
+        echo -e ${RED} "#### failed to build some targets (hh:mm:ss) ####"
+    fi
