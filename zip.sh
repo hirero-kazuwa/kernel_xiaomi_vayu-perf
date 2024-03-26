e6efa7a92615eb39773b6fd3effe3b514725b559
hirero-kazuwa <hirero00@gmail.com>
2024-03-26 07:49:43 +0530

    vayu: update zip.sh

--- zip.sh
+++ zip.sh
@@ -2,10 +2,10 @@
 
 LGR='\033[1;32m'
 
-anykernel="AnyKernel3"
-
-    echo "Copying files to $anykernel dir"
-    cp out/arch/arm64/boot/Image AnyKernel3 && cp out/arch/arm64/boot/dtbo.img AnyKernel3
-    cd AnyKernel3
-    zip -r franxx_vayu-kernel$(date +"%d%m%Y-%H%M").zip *
+    echo "Copying files to dir"
+    cp out/arch/arm64/boot/Image-dtb AnyKernel3 &&
+    cp out/arch/arm64/boot/dtbo.img AnyKernel3 &&
+    cp out/arch/arm64/boot/dts/qcom/sm8150-v2.dtb AnyKernel3/dtb.img &&
+    cd AnyKernel3 &&
+    zip -r franxx_vayu-kernel$(date +"%d%m%Y-%H%M").zip * &&
     mv *.zip /home/hirero
