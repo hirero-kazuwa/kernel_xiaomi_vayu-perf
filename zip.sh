8ab40fb218fa9ea94c7219ce435e28c587a16d10
hirero-kazuwa <hirero00@gmail.com>
2024-04-16 08:47:15 +0530

    vayu: update franxx version
    Signed-off-by: hirero-kazuwa <hirero00@gmail.com>

--- zip.sh
+++ zip.sh
@@ -3,9 +3,6 @@
 LGR='\033[1;32m'
 
     echo "Copying files to dir"
-    cp out/arch/arm64/boot/Image-dtb AnyKernel3 &&
-    cp out/arch/arm64/boot/dtbo.img AnyKernel3 &&
-    cp out/arch/arm64/boot/dts/qcom/sm8150-v2.dtb AnyKernel3/dtb.img &&
     cd AnyKernel3 &&
     zip -r franxx_vayu-kernel$(date +"%d%m%Y-%H%M").zip * &&
     mv *.zip /home/hirero
